89649e4ea259b19aee78adacc9e8a8b4a15b5d71
robiat <zakariyahrobiat2018@gmail.com>
2022-10-01 13:28:06 +0000

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-<<<<<<< HEAD
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-=======
-import { Context } from "../src/navbar/context";
-// import "./style.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Context>
-      <App />
-    </Context>
->>>>>>> 6633dcbeffe73c024f4bbffc292e1a38eb10db59
   </React.StrictMode>
 );
